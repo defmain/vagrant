1de15893839acb47ae2e8007f9ba6621693912b0
defmain <tilt+git@attaq.org>
2017-02-17 07:55:16 -0800

    Vagrant setup for two networked VMs

---
+++ two-networked-servers/bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt-get --yes update >/dev/null 2>&1
+sudo apt-get --yes upgrade >/dev/null 2>&1
+sudo apt-get --yes install vim tmux curl wget git
+
+# Add my .vimrc
+su vagrant -c "echo 'syntax on' >> ~/.vimrc"
+su vagrant -c "echo 'set tabstop=8' >> ~/.vimrc"
+su vagrant -c "echo 'set softtabstop=0' >> ~/.vimrc"
+su vagrant -c "echo 'set expandtab' >> ~/.vimrc"
+su vagrant -c "echo 'set smarttab' >> ~/.vimrc"
+su vagrant -c "echo 'set shiftwidth=2' >> ~/.vimrc"
+su vagrant -c "echo 'set cursorline' >> ~/.vimrc"
+su vagrant -c "echo 'set colorcolumn=80' >> ~/.vimrc"
+
